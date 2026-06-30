f09ee6326997c44e3eeb9c7448f7fc60113668ea
Truong Giang Pham <gorariagroup@gmail.com>
2026-06-30 18:50:25 +0700

    update

--- src/modules/cors.ts
+++ src/modules/cors.ts
@@ -3,7 +3,7 @@ import cors, { CorsOptions } from "cors";
 export const corsConfig = (options?: CorsOptions) => {
   const corsOptions: CorsOptions = {
     ...options,
-    origin: "http://localhost:3000",
+    // origin: "http://localhost:3000",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With", "Accept"],
